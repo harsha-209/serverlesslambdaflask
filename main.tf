bff31c57517a7710ae32522c17a4e66a69dff0a8
harsha-209 <nanduri.harshadsai@gmail.com>
2022-02-15 21:19:20 +0530

    completed

--- main.tf
+++ main.tf
@@ -159,31 +159,25 @@ resource "aws_api_gateway_integration_response" "example" {
     depends_on = [aws_api_gateway_method_response.response_200]
 }
 
-# aws_api_gateway_method_response.example:
-
-/*
-
-#resource "aws_api_gateway_method_response" "new" {
-#    http_method         = "GET"
-#    resource_id         = aws_api_gateway_resource.MyDemoResource.id
-#    response_models     = {
-#        "application/json" = "Empty"
-#    }
-#    response_parameters = {}
-#    rest_api_id         = aws_api_gateway_rest_api.example.id
-#    status_code         = "200"
-#
-
-*/
-
-
+##############deployment
 
 
 resource "aws_api_gateway_deployment" "example" {
   rest_api_id = aws_api_gateway_rest_api.example.id
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example.body))
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.MyDemoResource.id,
+      aws_api_gateway_method.MyDemoMethod,
+      aws_api_gateway_integration.integration.id,
+    ]))
   }
 
   lifecycle {
@@ -191,6 +185,10 @@ resource "aws_api_gateway_deployment" "example" {
   }
   depends_on = [aws_api_gateway_method.MyDemoMethod]
 }
+
+
+
+
 
 resource "aws_api_gateway_stage" "example" {
   deployment_id = aws_api_gateway_deployment.example.id
